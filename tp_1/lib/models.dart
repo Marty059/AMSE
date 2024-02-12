700c4378e7881d898253d59b789e88af6c677437
Martin <m.delsart67@gmail.com>
2024-02-12 17:05:52 +0100

    Images séries

--- tp_1/lib/models.dart
+++ tp_1/lib/models.dart
@@ -16,6 +16,26 @@ const series = [
     title: 'Stranger Things',
     realisateur: 'Test',
   ),
+  MediaModel(
+    imageUrl: 'assets/imgs/mandalorian.jpg',
+    title: 'The Mandalorian',
+    realisateur: 'Test',
+  ),
+  MediaModel(
+    imageUrl: 'assets/imgs/peaky-blinders.webp',
+    title: 'Peaky Blinders',
+    realisateur: 'Test',
+  ),
+  MediaModel(
+    imageUrl: 'assets/imgs/witcher.webp',
+    title: 'The Witcher',
+    realisateur: 'Test',
+  ),
+  MediaModel(
+    imageUrl: 'assets/imgs/la-casa-de-papel.webp',
+    title: 'La Casa de Papel',
+    realisateur: 'Test',
+  ),
 ];
 
 const films = [
