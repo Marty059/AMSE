16fdef0d7feb74a301d619d914084223fb800577
Géry <gery.bellanger@gmail.com>
2024-02-12 14:42:20 +0100

    Favorites in another folder

--- tp_1/lib/main.dart
+++ tp_1/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tp_1/src/books.dart';
+import 'package:tp_1/src/favorites.dart';
 
 void main() {
   runApp(MyApp());
@@ -154,35 +155,6 @@ class GeneratorPage extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text("No favourites yet hehe"),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        SizedBox(width: 10),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
     );
   }
 }
