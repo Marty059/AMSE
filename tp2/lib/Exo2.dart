c7e255471a4f7d8177c2880bda492c0582fc29ae
Géry <gery.bellanger@gmail.com>
2024-02-14 11:50:49 +0100

    louche

---
+++ tp2/lib/Exo2.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:tp2/main.dart';
+import 'package:flutter/material.dart';
+
+class Exo1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Image App'),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Center(
+              child: Image.network(
+            'https://picsum.photos/512/1024',
+          ));
+        },
+      ),
+    ));
+  }
+}
+
+/// Flutter code sample for [Slider].
+
+void main() => runApp(const SliderApp());
+
+class SliderApp extends StatelessWidget {
+  const SliderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SliderExample(),
+    );
+  }
+}
+
+class SliderExample extends StatefulWidget {
+  const SliderExample({super.key});
+
+  @override
+  State<SliderExample> createState() => _SliderExampleState();
+}
+
+class _SliderExampleState extends State<SliderExample> {
+  double _currentSliderValue = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Slider')),
+      body: Slider(
+        value: _currentSliderValue,
+        max: 100,
+        divisions: 5,
+        label: _currentSliderValue.round().toString(),
+        onChanged: (double value) {
+          setState(() {
+            _currentSliderValue = value;
+          });
+        },
+      ),
+    );
+  }
+}
