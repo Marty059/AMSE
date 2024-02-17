dbf8e92e6528d0947e36209a05a5227a17cdb9de
Géry <gery.bellanger@gmail.com>
2024-02-17 19:31:36 +0100

    Exo 2 fait pour ordi

--- tp2/lib/Exo2.dart
+++ tp2/lib/Exo2.dart
@@ -24,7 +24,10 @@ class Exo2 extends StatefulWidget {
 }
 
 class _Exo2State extends State<Exo2> {
-  double _currentSliderValue = 20;
+  double _currentSliderValueX = 0;
+  double _currentSliderValueZ = 0;
+  bool _currentFlipValueX = false;
+  double _currentSliderValueScale = 10;
 
   @override
   Widget build(BuildContext context) {
@@ -33,19 +36,97 @@ class _Exo2State extends State<Exo2> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network(
-              'https://picsum.photos/512/1024',
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Transform(
+                transform: Matrix4.identity()
+                  ..rotateX(_currentSliderValueX * (3.141592653589793 / 180))
+                  ..rotateZ(_currentSliderValueZ * (3.141592653589793 / 180))
+                  ..scale(_currentSliderValueScale / 10)
+                  ..scale(_currentFlipValueX ? -1.0 : 1.0),
+                alignment: Alignment.center,
+                child: Container(
+                  child: Image.network(
+                    'https://picsum.photos/512/1024',
+                  ),
+                ),
+              ),
             ),
-            Slider(
-              value: _currentSliderValue,
-              max: 100,
-              divisions: 5,
-              label: _currentSliderValue.round().toString(),
-              onChanged: (double value) {
-                setState(() {
-                  _currentSliderValue = value;
-                });
-              },
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(children: [
+                Text("Rotate X"),
+                Expanded(
+                  child: Slider(
+                    value: _currentSliderValueX,
+                    max: 360,
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderValueX = value;
+                      });
+                    },
+                  ),
+                ),
+              ]),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(children: [
+                Text("Rotate Z"),
+                Expanded(
+                  child: Slider(
+                    value: _currentSliderValueZ,
+                    min: 0,
+                    max: 360,
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderValueZ = value;
+                      });
+                    },
+                  ),
+                ),
+              ]),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(children: [
+                Text("Mirror"),
+                Checkbox(
+                    value: _currentFlipValueX,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _currentFlipValueX = !_currentFlipValueX;
+                      });
+                    }),
+              ]),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(children: [
+                Text("Scale"),
+                Expanded(
+                  child: Slider(
+                    value: _currentSliderValueScale,
+                    min: 0,
+                    max: 100,
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderValueScale = value;
+                      });
+                    },
+                  ),
+                ),
+              ]),
             ),
           ],
         ),
